baa05b20ae0ab518824ecaca2da7f80f96c4dfd0
MAXSUNS <sundai.cool@hotmail.com>
2018-01-12 16:20:47 +0800

    add send email

--- services/messageService.js
+++ services/messageService.js
@@ -1,10 +1,16 @@
 
 var Promise = require("bluebird");
 var request = Promise.promisifyAll(require("request"));
-function sendMessage(service,url) {
+const nodemailer = require('nodemailer');
+
+function sendMessage(content,url) {
+    this.sendDingMessage(content,url)
+}
+
+function sendDingMessage(content,url) {
     var data = {
         msgtype: 'text',
-        text: {"content": service}
+        text: {"content": content}
     }
     var options = {
         url: url,
@@ -13,6 +19,45 @@ function sendMessage(service,url) {
     }
     request.postAsync(options);
 }
+
+function sendEmailMessage(content) {
+    nodemailer.createTestAccount((err, account) => {
+
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: account.user, // generated ethereal user
+                pass: account.pass  // generated ethereal password
+            }
+        });
+
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
+            to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
+            subject: 'Hello ✔', // Subject line
+            text: 'Hello world?', // plain text body
+            html: '<b>Hello world?</b>' // html body
+        };
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+            // Preview only available when sending through an Ethereal account
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
+            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        });
+    });
+}
+
 module.exports = {
     sendMessage: sendMessage
 }
