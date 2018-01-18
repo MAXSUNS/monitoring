95f78cfe3056b6b60c1da1d378c60c61fa59b20b
MAXSUNS <sundai.cool@hotmail.com>
2018-01-18 17:27:13 +0800

    add send email

--- init.js
+++ init.js
@@ -12,6 +12,20 @@ function initAll() {
     for (let environment of environments) {
         setInterval(() => checkService.getEurekaInfo(environment,path.join(__dirname, 'public/eureka/')),"10000");
     }
+
+    /**
+     * 读取默认配置文件
+     */
+    const content = fs.readFileSync(path.join(__dirname, 'config', 'config.json'));
+    const defaultConfig = JSON.parse(content);
+    let envConfig;
+
+    /**
+     * 合并配置项
+     * 设置配置为全局变量
+     */
+    global.config || ( global.config = (envConfig ? _.merge({}, defaultConfig, envConfig) : defaultConfig ));
+
 }
 
 module.exports = {
--- services/messageService.js
+++ services/messageService.js
@@ -29,18 +29,18 @@ function sendEmailMessage(content) {
             port: 587,
             secure: false, // true for 465, false for other ports
             auth: {
-                user: account.user, // generated ethereal user
-                pass: account.pass  // generated ethereal password
+                user: config.email.account, // generated ethereal user
+                pass: config.email.password  // generated ethereal password
             }
         });
 
         // setup email data with unicode symbols
         let mailOptions = {
-            from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
-            to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
+            from: 'monitoring', // sender address
+            to: config.email.receivers, // list of receivers
             subject: 'Hello ✔', // Subject line
-            text: 'Hello world?', // plain text body
-            html: '<b>Hello world?</b>' // html body
+            text: content, // plain text body
+            html: '<b>'+content+'</b>' // html body
         };
 
         // send mail with defined transport object
@@ -49,11 +49,7 @@ function sendEmailMessage(content) {
                 return console.log(error);
             }
             console.log('Message sent: %s', info.messageId);
-            // Preview only available when sending through an Ethereal account
             console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
-            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
         });
     });
 }
